025322dbece2cc85cc419360ad5fc5f5d69646af
davona123 <zerc3251@gmail.com>
2024-10-31 22:43:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Portugues',
+    'O que é um substantivo?',
+    'Substantivo é a palavra que nomeia pessoas, lugares, coisas ou ideias.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um bioma?',
+    'Grande área com características climáticas e ecológicas semelhantes.'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é um número primo?',
+    'Um número primo é um número natural maior que 1 que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Ciencias',
+    'O que é a fotossíntese?',
+    'Fotossíntese é o processo pelo qual as plantas convertem luz solar, água e dióxido de carbono em energia e oxigênio.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'A Revolução Francesa foi um movimento social e político que começou em 1789, visando acabar com a monarquia e estabelecer uma república.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é célula?',
+    'Célula é a unidade básica da vida, que compõe todos os seres vivos.'
+)
